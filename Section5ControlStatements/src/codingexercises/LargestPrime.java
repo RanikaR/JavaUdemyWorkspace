1d18502da4f3f1f5d7feeca7ffcc8261f8555c50
RanikaR <rmr29@njit.edu>
2020-09-11 16:52:53 -0400

    Completed Coding Exercise 25: Largest Prime

--- Section5ControlStatements/src/codingexercises/LargestPrime.java
+++ Section5ControlStatements/src/codingexercises/LargestPrime.java
@@ -5,8 +5,64 @@ package codingexercises;
 public class LargestPrime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println(getLargestPrime(21));
+		System.out.println(getLargestPrime(217));
+		System.out.println(getLargestPrime(0));
+		System.out.println(getLargestPrime(45));
+		System.out.println(getLargestPrime(-21));
 
 	}
+	
+	public static int getLargestPrime(int number) {
+		
+		if (number <= 1) {
+			return -1;
+		}
+		
+		// Find Factors by starting with largest and going to smallest
+		for (int i = (number/2); i > 1; i-- ) {
+			
+			if (number % i == 0 ) {
+				
+				// Check if current Factor is Prime
+				if (number % i == 0) {
+					number = i;
+				}
+				
+			}
+			
+		}
+		return number;
+	}
+	
+//	public static boolean isPrime(int n) {
+//		if (n == 1) {
+//			return false;
+//		}
+//		
+//		for(int i = 2; i <= n/2; i++) {   // Optimized: for(int i = 2; i <= (long) Math.sqrt(n); i++)
+//			if(n % i == 0) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//	
+//	public static void printFactors(int number) {
+//		
+//		if (number < 1) {
+//			System.out.println("Invalid Value");
+//		}
+//		
+//		for (int i = 1; i <= number; i++) {
+//			
+//			if(number % i == 0) {
+//				System.out.println(i);
+//			}
+//			
+//		}
+//		
+//	}
 
 }
