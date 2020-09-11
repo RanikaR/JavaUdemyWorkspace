bc6bbf996b5ff3a1f9750b8261073038d01b589b
RanikaR <rmr29@njit.edu>
2020-09-11 17:22:25 -0400

    First commit Coding Exercise 26: Diagonal Star

---
+++ Section5ControlStatements/src/codingexercises/DiagonalStar.java
@@ -0,0 +1,21 @@
+package codingexercises;
+
+public class DiagonalStar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void printSquareStar(int number) {
+		
+		if (number < 5) {
+			System.out.println("Invalid Value");
+		}
+		
+		
+		
+		
+	}
+
+}
