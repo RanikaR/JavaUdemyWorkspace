54186da201918d279f03fc70110bef093431f713
RanikaR <rmr29@njit.edu>
2020-10-22 12:20:59 -0400

    Lesson 71. Reading User Input

---
+++ Section5ControlStatements/src/lessons/ReadingUserInput.java
@@ -0,0 +1,15 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class ReadingUserInput {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String name = scanner.nextLine();
+		scanner.close();
+		System.out.println("Hello, " + name);
+	}
+
+}
